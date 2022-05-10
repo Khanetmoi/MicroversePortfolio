56172dd8cd2826cefc5a65b58b9155561fc062ed
khan <khanetmoi@gmail.com>
2022-05-10 14:18:41 +0000

    mobile menu
    click on the hambuger button

---
+++ javascript/mobile.js
@@ -0,0 +1,35 @@
+const contactList = document.getElementById('contactList');
+const hambugerButton = document.getElementById('hamburgerButton');
+const hambugerButton2 = document.getElementById('hamburgerButton2');
+const options = document.getElementsByClassName('options');
+
+hambugerButton.addEventListener('click', () => {
+  contactList.classList.toggle('appear');
+  // if(screen.Width > 776)
+  // {
+  //     contactList.style.backgroundImage = "none";
+  // }
+  document.body.style.overflow = 'hidden';
+});
+
+hambugerButton2.addEventListener('click', () => {
+  contactList.classList.remove('appear');
+  document.body.style.overflow = 'visible';
+});
+
+console.log(options);
+
+options[0].addEventListener('click', () => {
+  contactList.classList.remove('appear');
+  document.body.style.overflow = 'visible';
+});
+
+options[1].addEventListener('click', () => {
+  contactList.classList.remove('appear');
+  document.body.style.overflow = 'visible';
+});
+
+options[2].addEventListener('click', () => {
+  contactList.classList.remove('appear');
+  document.body.style.overflow = 'visible';
+});
